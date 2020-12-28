a9930e3a0914afad43bf3954940b5042ee8b7be7
kamigo777 <kaminsky777@gmail.com>
2020-12-28 09:07:32 +0200

    1

---
+++ samplesite/bboard/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from.models import Bb
+from.models import Rubric
+admin.site.register(Rubric)
+
+class BbAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published', 'rubric')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content', )
+
+admin.site.register(Bb)
+
+# Register your models here.
